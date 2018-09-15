d5004ba31ec4203006aaf7c19c12aa6f1d61512d
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2018-09-15 17:34:07 +0300

    Update and rename day-2-operators.cpp to day-02-operators.cpp

--- cpp/day-02-operators.cpp
+++ cpp/day-02-operators.cpp
@@ -10,18 +10,14 @@ void solve(double meal_cost, int tip_percent, int tax_percent) {
     printf("The total meal cost is %d dollars.\n", totalCost);
 }
 
-int main()
-{
-    double meal_cost;
-    cin >> meal_cost;
+int main() {
+    double meal_cost; cin >> meal_cost;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    int tip_percent;
-    cin >> tip_percent;
+    int tip_percent; cin >> tip_percent;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    int tax_percent;
-    cin >> tax_percent;
+    int tax_percent; cin >> tax_percent;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     solve(meal_cost, tip_percent, tax_percent);
