f78b52d718463e7689956030e6b5406e1ec47def
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2018-09-15 17:43:02 +0300

    Update and rename day-9-recursion-3.cpp to day-09-recursion-3.cpp

--- cpp/day-09-recursion-3.cpp
+++ cpp/day-09-recursion-3.cpp
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// Complete the factorial function below.
-int factorial(int n)
-{    
+int factorial(int n) {    
     return (n <= 1) ? 1 : n * factorial(n-1);
 }
 
-int main()
-{
+int main() {
     ofstream fout(getenv("OUTPUT_PATH"));
 
-    int n;
-    cin >> n;
+    int n; cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     int result = factorial(n);
-
     fout << result << "\n";
-
     fout.close();
 
     return 0;
