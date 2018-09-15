1c5bd5dd374ff12601903c5c51e2483ca852a040
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2018-09-15 17:03:14 +0300

    Create day-19-interfaces.cpp

---
+++ cpp/day-19-interfaces.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n) = 0;
+};
+
+class Calculator : public AdvancedArithmetic {
+    public:
+        int divisorSum(int n) {
+            int divisorSum = 0;
+            for (int i=1; i<=sqrt(n); i++) {
+                if (n%i == 0){
+                    if (n/i == i)
+                        divisorSum += i;
+                    else
+                        divisorSum += i + n/i;
+                }
+            }
+            return divisorSum;
+        }
+};
+
+int main() {
+    int n; cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
